ab0cd9cffecea8ae78c98ba72afcf214ba445526
Berke Sayın <sayinberkesayin@gmail.com>
2021-09-14 20:24:19 +0300

    learned about web developer tools on browser and how to use it and the console.

---
+++ 4.javascript-codes/1.js-intro-part-1/2.web-developer-tools/app.js
@@ -0,0 +1,14 @@
+// console.log("Merhaba Javascript");
+// console.log(true);
+// console.log(false);
+
+// var a = 10;
+// console.log(a);
+
+// console.log(["Ahmet", "Mehmet"]);
+
+console.log(typeof a);
+
+console.warn("bu bir uyarıdır.")
+console.error("bu sayfa bulunamadı.")
+console.clear();
