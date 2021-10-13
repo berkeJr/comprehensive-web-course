70178168018b7cda373389d72e13febdc774f03a
Berke Sayın <sayinberkesayin@gmail.com>
2021-10-13 13:13:14 +0300

    Create elements dynamically.

--- 4.javascript-codes/18.to-do-list-project/todo.js
+++ 4.javascript-codes/18.to-do-list-project/todo.js
@@ -25,10 +25,49 @@ const clearButton = document.querySelector("#clear-todos");
 /* Sonra ise, biz burada inputa girilen to do ları ekleyeceğimiz için buradaki forma bie submit olayı kazandırmamız lazım.
 Yani form submit olduğunda girilen todo, list-item alanına eklenmeli */
 
-// aşağıda oluşturduğumuz fonksiyonu çalışması için burada çağıralım.
+// aşağıda oluşturduğumuz fonksiyonu çalışması için burada çağıralım. 
 eventListeners();
 
 // bütün eventListener'ları yöneteceğimiz bir fonksiyon oluşturalım.
 function eventListeners() {
-    
+    form.addEventListener("submit", addTodo); // submit olunca todo'nun list-item'a eklenmesi için gerekli olan fonksiyon
+}
+
+// submit olunca todo'nun list-item'a eklenmesi için gerekli olan fonksiyon
+function addTodo(e) {
+
+    // ilk önce todo ekleme input'undaki değere ulaşmamız gerekiyor.
+    const newTodo = todoInput.value.trim();
+    // not: js trim function: string'in başındaki ve de sonundaki boşlukları siler. 
+
+    // bu aldığımız newTodo'yu list item olarak ekleyeceğiz (ul içerisine)  yani arayüze eklenecek (UI)
+    addTodoToUI(newTodo);
+
+    // formumuz submit olduğunda tekrardan sayfaya yönelmesin diye default olan özelliğini değiştirelim
+    e.preventDefault();
+}
+
+function addTodoToUI(newTodo) {  // bu fonksiyon aldığı string değerini list item olarak UI'a ekleyecek
+
+    /**  // String Değer
+     * 
+        <li class="list-group-item d-flex justify-content-between">
+            Todo 1
+            <a href = "#" class ="delete-item">
+                <i class = "fa fa-remove"></i>
+            </a>
+        </li> 
+    */
+
+        // li elementini dinamik bir şekilde oluşturalım
+        const ListItem = document.createElement("li");
+        ListItem.className = "list-group-item d-flex justify-content-between";
+        
+
+        // a elementini (link) dinamik bir şekilde oluşturalım
+        const link = document.createElement("a");
+        link.href = "#";
+        link.className = "delete-item";
+        link.innerHTML = "<i class = 'fa fa-remove'></i>"
+
 }
