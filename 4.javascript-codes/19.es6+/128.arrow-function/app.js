1e3fb932acbafd7813773a5696374919bf3f4da9
Berke Sayın <sayinberkesayin@gmail.com>
2021-11-28 16:35:07 +0300

    Create an arrow function.

---
+++ 4.javascript-codes/19.es6+/128.arrow-function/app.js
@@ -0,0 +1,23 @@
+// Bu bölümümüzde EcmaScript6 ile ve daha sonra diğer standartla gelen JavaScript'in yeni standartlarını öğreneceğiz. 
+
+/* Arrow Functions; EcmaScript6 standartları ile gelen, b,,zm fonksiyonları yazmamızı kısaltan ve kolaylaştıran aslında güzel bir
+söz dizimi özelliği. Askında arka planda işlerimiz yine aynı. */
+
+// Js'de fonksiyon oluşturma yöntemi. (merhaba() isminde bir fonksiyon)
+const merhaba = function() {
+    console.log("Merhaba!");
+}
+
+merhaba();  // fonksiyonu çağıralım. 
+
+// Arrow function ile fonksiyon oluşturma
+const selam = () => {
+    console.log("Selam!");
+}
+
+selam();
+
+// Aslında pek fark gözükmese de tek satırda yazmak için oldukça kolaylık sağlar. 
+
+
+
