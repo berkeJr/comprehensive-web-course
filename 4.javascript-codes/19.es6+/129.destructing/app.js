be2b15e07c945b5246cea2d5db38c562e32d6c95
Berke Sayın <sayinberkesayin@gmail.com>
2021-11-30 19:08:23 +0300

    Create an arrow function and use function destructing.

--- 4.javascript-codes/19.es6+/129.destructing/app.js
+++ 4.javascript-codes/19.es6+/129.destructing/app.js
@@ -1,7 +1,7 @@
 // Destructing
 
-/* Destructing aslında bizim array'lerimizin ya da object'lerimizin içinden değerlerimizi almak  için kullandığımız kısa bir yol 
- * ve bunu kullanarak bazı işlemlerimizi daha da kısaltabiliriz. */
+/* Destructing aslında bizim array'lerimizin ya da object'lerimizin veya fonksiyonlarımızın içinden değerlerimizi almak  için 
+kullandığımız kısa bir yol ve bunu kullanarak bazı işlemlerimizi daha da kısaltabiliriz. */
 
 // 4 adet değişken tanımlayalım. Değerlerini daha sonra verebiliriz.
 let number1, number2, number3, number4; 
@@ -51,3 +51,14 @@ const {a: number7, c: number8, e: number9} = numbers;
 
 console.log(number7, number8, number9); 
 
+
+// Function Destructing
+const getLangs = () => ["Python", "Java", "C++"];   // Tek bir return yapacağı için return yazmaya gerek yok. 
+// Burada Fonksiyon sadece bir tane array dönüyor. 
+
+// Dönen array içerisindeki Python, Java, C++'ı 3 tane değişkene atayalım. 
+
+const [lang1, lang2, lang3] = getLangs(); 
+// lang1 = Python, lang2 = Java, lang3 = C++ şeklinde atama yapıldı. 
+
+console.log(lang1, lang2, lang3);
