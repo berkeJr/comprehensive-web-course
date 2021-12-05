200dc4c08c35fd6d708dc960bac81b5a6cbff801
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-05 15:52:54 +0300

    Explain the differences between Reference Types and Primitive Types in Javacript.

---
+++ 4.javascript-codes/19.es6+/133.reference-types/app.js
@@ -0,0 +1,33 @@
+// Primitive Types and Reference Types in JavaScript
+
+// Facts
+
+/*
+ * -- JavaScript has 2 kinds of variable types: primitive and reference:
+ * -- A fixed amount of memory is reserved after creation of every variable.
+ * -- When a variable is copied, it's in-memory value is copied.
+ * -- Passing a variable to a function via a call also creates a copy of that variable.
+ */
+
+
+/*  Primitive Types: 
+ *  The in-memory value of a primitive type is it's actual value (e.g. boolean true, number 42). 
+ *  A primitive type can be stored in the fixed amount of memory available. 
+ *  null
+ *  undefined
+ *  Boolean
+ *  Number
+ *  String
+ * Primitive types are also known as: scalar types or simple types.
+ */
+
+
+/** Reference Types
+ *  A reference type can contain other values. Since the contents of a reference type can not fit in the fixed amount 
+ *  of memory available for a variable, the in-memory value of a reference type is the reference itself (a memory address).
+ *  Array
+ *  Object
+ *  Function
+ *  Reference types are also known as: complex types or container types.
+ */
+
