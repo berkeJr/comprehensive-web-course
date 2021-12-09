e9577a650cd32bf2abbf8e0a0da2b5fc1d0f4cee
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-09 17:28:34 +0300

    Start intro.

---
+++ 5.react-course/intro/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+        <h3>Hi there! It's Berke, Welcome to React!</h3>
+    </div>
+  );
+}
+
+export default App;
