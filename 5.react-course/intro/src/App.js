8bead04ce339fe079b691bb89451fe616fa47151
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-11 16:49:12 +0300

    İmport reactstrap and use it.

--- 5.react-course/intro/src/App.js
+++ 5.react-course/intro/src/App.js
@@ -1,36 +1,39 @@
-import React from 'react'
-import Navi from './Navi'  
-import CategoryList from './CategoryList';
-import ProductList from './ProductList';
-
-
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
   return (
     // Navi componentini burada kullanabiliriz. (div içerisinde)
     <div>
-         <Navi></Navi>
-         <Navi/>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
 
-         <CategoryList></CategoryList>
-         <CategoryList/>
-
-         <ProductList></ProductList>
-         <ProductList/>
-
+          <Col xs="9">
+            <ProductList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-  /**Burada return işlemi direkt olarak bir parantezle başlıyor. Burada bir .jsx yapısı var.  Yani JavaScript XML 
+  /**Burada return işlemi direkt olarak bir parantezle başlıyor. Burada bir .jsx yapısı var.  Yani JavaScript XML
    * dediğimiz yapı var. Bu react'ta jsx yapısıdır. Peki nedir bu jsx yapısı? React aslında bir kütüphane ve arayüz
-   * oluşturmaya yönelik kolaylıklar sağlıyor. O da react'ın en önemli yapısı. Yani .jsx yapısı. Normalde return 
-   * içerisindeki ifadenin HTML'den bir farkı yok gibi gelebilir. Buradaki div aslında HTML olarak render ediliyor, 
+   * oluşturmaya yönelik kolaylıklar sağlıyor. O da react'ın en önemli yapısı. Yani .jsx yapısı. Normalde return
+   * içerisindeki ifadenin HTML'den bir farkı yok gibi gelebilir. Buradaki div aslında HTML olarak render ediliyor,
    * yani biz html'deki div'i ve diğer elementleri aynen burada da kullanabiliyoruz, ama aslında buradaki yapı tam olarak
    * jsx yapısıdır. Yani bu div'in react kütüphanesinde bir karşılığı var. Ayrıca örneğin biz bu diV'in altında, dışında
-   * bir h3 elementi oluşturursak hata verir. Örneğin bir div elementi var açılıp kapanıyor, biz onunla aynı hiyerarşide 
-   * yani ona kardeş bir h3 element oluşturamayız. Yani bizim bir ana container tag'ımız olacak(div) biz onun içerisine 
-   * yazacağız. 
+   * bir h3 elementi oluşturursak hata verir. Örneğin bir div elementi var açılıp kapanıyor, biz onunla aynı hiyerarşide
+   * yani ona kardeş bir h3 element oluşturamayız. Yani bizim bir ana container tag'ımız olacak(div) biz onun içerisine
+   * yazacağız.
    */
-   
 }
 
 export default App;
--- 5.react-course/intro/src/CategoryList.js
+++ 5.react-course/intro/src/CategoryList.js
@@ -1,13 +1,22 @@
-// rcc + enter ile CategoyList isimli class componentini oluşturalım
+import { ListGroupItem, ListGroup } from "reactstrap";
 
-import React, { Component } from 'react'
+// rcc + enter ile CategoyList isimli class componentini oluşturalım
+import React, { Component } from "react";
+
 
 export default class CategoryList extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Category List</h3>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Category List</h3>
+        <ListGroup>
+          <ListGroupItem>Cras justo odio</ListGroupItem>
+          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+          <ListGroupItem>Morbi leo risus</ListGroupItem>
+          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+        </ListGroup>
+      </div>
+    );
+  }
 }
