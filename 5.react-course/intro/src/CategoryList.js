3d449aeb36d51c2d118f1babb9d28b7ec5d922cd
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-15 17:47:54 +0300

    Learn state management.

--- 5.react-course/intro/src/CategoryList.js
+++ 5.react-course/intro/src/CategoryList.js
@@ -3,25 +3,29 @@ import { ListGroupItem, ListGroup } from "reactstrap";
 // rcc + enter ile CategoyList isimli class componentini oluşturalım
 import React, { Component } from "react";
 
-
 export default class CategoryList extends Component {
-
   constructor(props) {
     super(props);
-    state:{}
+    // this.state = { counter:1} // counter bir değişken ismi
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    }; // categori'ler bizim için boş bir array.
   }
 
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
-       
+
+        {/* <h3>{this.state.counter}</h3> */}
+
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem key={category.categoryId} >{category.categoryName}</ListGroupItem>
+          ))}
         </ListGroup>
       </div>
     );
