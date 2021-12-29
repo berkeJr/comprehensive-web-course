0fa3c14b8688227158b55eab94be8c67cf3cb66b
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-29 13:35:11 +0300

    Create increaseCounter() function for INCREASE_COUNTER action.

--- 5.react-course/redux-intro/src/redux/actions/counterActions.js
+++ 5.react-course/redux-intro/src/redux/actions/counterActions.js
@@ -1,2 +1,16 @@
 import * as actionTypes from "./actionTypes";
 
+export const increaseCounter = () => ({  // fonksiyonun değeri de bir fonksiyon, parametre obje. 
+    type:actionTypes.INCREASE_COUNTER,  
+    payload:1
+})
+// yani aksiyonumuz bir fonksiyon ve o fonksiyonun parametresi de süslü parantez içerisinde yazılanlar(obje). 
+
+// Biz daha sonra bu aksiyonları redux'ta başka bir yerde çalıştıracağız.(reducer)
+
+/** Şimdi yapı şöyle olacak: 
+ * Sayının gösterildiği component,
+ * Sayacı 1 arttıran buton component
+ * Sayacı 2 arttıran buton component
+ * Sayacı 1 azaltan buton component
+ */
