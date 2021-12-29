25282cdcafbed3812a248d1e7acb2e9f7a0739c6
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-29 13:21:58 +0300

    Define actions at the actionTypes.js file.

---
+++ 5.react-course/redux-intro/src/redux/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const INCREASE_COUNTER = "INCREASE_COUNTER";
+// Bu 1 arttırma işlemi, aslında değişken olarak düşünebiliriz.
+
+export const DECREASE_COUNTER = "DECREASE_COUNTER";
+// Bu 1 azaltma işlemi.
+
+export const INCREASE_BY_TWO_COUNTER = "INCREASE_BY_TWO_COUNTER";
+// Bu da 2 arttırma aksiyonu.
