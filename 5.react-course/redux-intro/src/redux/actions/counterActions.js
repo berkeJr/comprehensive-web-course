6564facdedb823472fe5383d3f3d4daec4d91319
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-29 13:39:05 +0300

    Write actions' funcitons.

--- 5.react-course/redux-intro/src/redux/actions/counterActions.js
+++ 5.react-course/redux-intro/src/redux/actions/counterActions.js
@@ -1,16 +1,30 @@
 import * as actionTypes from "./actionTypes";
 
-export const increaseCounter = () => ({  // fonksiyonun değeri de bir fonksiyon, parametre obje. 
-    type:actionTypes.INCREASE_COUNTER,  
-    payload:1
-})
-// yani aksiyonumuz bir fonksiyon ve o fonksiyonun parametresi de süslü parantez içerisinde yazılanlar(obje). 
+// yani aksiyonumuz bir fonksiyon ve o fonksiyonun parametresi de süslü parantez içerisinde yazılanlar(obje).
+export const increaseCounter = () => ({
+  // fonksiyonun değeri de bir fonksiyon, parametre obje.
+  type: actionTypes.INCREASE_COUNTER,
+  payload: 1,
+});
+
+export const decreseCounter = () => ({
+  type: actionTypes.DECREASE_COUNTER,
+  payload: 1,
+});
+
+export const increaseByTwoCounter = () => ({
+  type: actionTypes.INCREASE_BY_TWO_COUNTER,
+  payload: 1,
+});
 
 // Biz daha sonra bu aksiyonları redux'ta başka bir yerde çalıştıracağız.(reducer)
 
 /** Şimdi yapı şöyle olacak: 
  * Sayının gösterildiği component,
- * Sayacı 1 arttıran buton component
+ 
+ * Sayacı 1 arttıran buton component: increaseCounter() fonksiyonunu çağıracak. 
+ * Daha sonra increaseCounter reducer'a gidecek ve bana şu type ve payload ile bir talep geldi, ve ona göre işlem yapılacak.
+
  * Sayacı 2 arttıran buton component
  * Sayacı 1 azaltan buton component
  */
