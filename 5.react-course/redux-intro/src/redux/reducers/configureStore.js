1ba07882805b14052685365dcffdd726706d1317
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-29 19:02:21 +0300

    create export default function at the configureStore.js.

--- 5.react-course/redux-intro/src/redux/reducers/configureStore.js
+++ 5.react-course/redux-intro/src/redux/reducers/configureStore.js
@@ -2,4 +2,13 @@ import { createStore } from "redux";
 
 import reducers from "./index";
 
+export default function configureStore() {
+    return createStore(reducers);
+}
 
+// createStore ile bir mağaza oluşturuyoruz ve oraya da index'teki reducers'ı yolluyoruz. 
+
+/** Yani configureStore'da olay şu: Bir mağaza olduğunu düşünelim. Yani bir state veri tabanım var. O state veri tabanının
+ * reducer'ları kimler. Yani state'i yöneten nesneleri kimler? İşte bu noktada bizim reducer'ımızda combine ettiğimiz
+ * tüm reducer'lar burada mevcut.
+ */
