7b64d5222435257a713ab6576c250e5d7ea99525
Berke Sayın <sayinberkesayin@gmail.com>
2022-07-24 17:47:33 +0300

    Study var, let, const.

--- 4.javascript/13.js-intro-part-1/88.variables/app.js
+++ 4.javascript/13.js-intro-part-1/88.variables/app.js
@@ -1,53 +1,102 @@
-/* javascript son yıllarda hem Türkiye'de hem de dünyada çok popüler bir dil haline geldi. Bunun sebebi dilin sürekli olarak güncellenmesi ve de
-yeni özellikler ile yeni standartların dile sürekli olarak eklenmesini örnek gösterebiliriz. Ancak js ilk başlarda sadece DOM işlemleri için
-sadece web sayfalarında kullanılıyordu. Ancak bi js'de node.js gibi framework'ler sayesinde server tarafında da kullanabiliyoruz ve de
-çalıştırabiliyoruz.*/
+// 88.Değişken Tanımlama (var, let, const keywords)
 
+/* 
 
-/*Javascript'in 2015'den sonra gelen standartları: EcmaScript6(ES6) standartları sayesinde js'de biz bir değişkenimizi hem let anahtar 
-kelimesiyle hem de const anahtar kelimesiyle tanımlayabiliyoruz. Bu derste var, let ve const farklılıklarına bakacağız.*/
+- Javascript son yıllarda hem Türkiye'de hem de dünyada çok popüler bir dil haline geldi. Bunun sebebi olarak dilin sürekli olarak
+güncellenmesini ve de yeni özellikler ile yeni standartların dile sürekli olarak eklenmesini örnek gösterebiliriz. 
+Ancak js ilk başlarda sadece DOM işlemleri için yani sadece web sayfalarında kullanıcıların gördüğü alanda (client-side) 
+kullanılıyordu. Ancak artık biz js'yi node.js gibi framework'ler sayesinde server tarafında da kullanabiliyoruz.
 
-/*Var : biz var ile istersek değişkeni tanımlarken istersek de değişkeni tanımladıktan sonra alt satırlarda değer atama işlemini
-gerçekleştirebiliriz. ya da ilk değeri atanmış bir değişkenin değerini alt satırlarda farklı bir değer vererek değerini değişttirebiliriz*/
+*/
+
+// EcmaScript6 (ES6+) Standards:
+
+/* 
+
+- JavaScript'in 2015'den sonra gelen EcmaScript6(ES6) standartları sayesinde js'de biz bir değişkenimizi var keyword'üne ek olarak 
+let, const anahtar kelimeleriyle de tanımlayabiliyoruz. Bu derste var, let ve const farklılıklarına bakalım.
+
+*/
+
+/*   var keyword: 
+
+- var globally scoped veya function / locally scoped olarak kullanılabilir. Bu da şu anlama gelir: Herhangi bir yerde
+var ile tanımlanan bir değişkene biz yine herhangi bir yerde erişebiliriz.
+
+-- var variables can be re-declared and updated:
+
+- var ile tanımlanıp değeri atanmış bir değişken bir değişken daha sonra yine var ile başka bir yerde tanımlanıp yeni değer 
+verilebilir veya var ile tanımlanmadan değeri yine başka bir yerde değiştirilebilir. 
+
+var greeter = "hey hi";
+var greeter = "say Hello instead";   // tekrar tanımladığımızda herhangi bir hata olmaz.
+
+var greeter = "hey hi";
+greeter = "say Hello instead";    // değişkenin değeri güncellendiğinde de herhangi bir hata olmaz.
+
+console.log(greeter);
+
+*/
+
 var a = 20;
 console.log(a);
 
-/*let anahtar kelimesinin var'dan bir farkını gösterelim.*/ 
-let name = "Mustafa Murat";
-console.log(name);
+var a = 30;
 
-name = "Murat";
-console.log(name);
-/*bu şekilde let ile tanımlanmış bir değişkene alt satırda farklı bir değer atayabiliriz fakat let ile tanımlanmış bir değişkeni tekrardan
-alt satırlarda let ile tanımlayıp farklı bir değer atamaya çalışırsak bir hata alırız
-let name = "Murat";  // burada hata alırız*/ 
+a = 45;
 
-let b,c;
-b=10, c=20;
-console.log(a+b);
-
-
-/*const anahtar kelimesi. ingilizce'deki constant(sabit) kelimesinden geliyor. yani biz const ile bir değişkene değer atadığımız zaman
-daha sonra o değişkenin değerini alt satırlarda değiştiremiyoruz.
-const name="Mustafa Murat";
-console.log(name);
-
-name="Murat";
-console.log(name);
-bu şekilde bir şey denersek hata alırız. çünkü const ile tanımlanmış bir değişkenin değeri sabittir.
-*/ 
-
-/*ayrıca const ile tanımlanmış bir değişkenin değeri tanımlanırken atanmalıdır, değişkeni tanımlayıp alt satırlarda değer vermek istersek
-yine hata alırız
-const a;
-a=25;
 console.log(a);
-bu şekilde bir şey denersek yine hata alırız.
-*/ 
 
-const d = [1,2,3,4,5];
-console.log(d);
 
-//array'e yeni bir eleman eklemek için: push() fonksiyonunu kullanırız.
-d.push(6);
-console.log(d);
+
+/*   let keyword:
+
+- let block scoped keyword'dür. Yani bir değişken sadece tanımlandığı block içerisinde erişilebilir. 
+
+-- let can be updated but not re-declared:
+
+- let ile tanımlanıp değeri atanmış bir değişkenin daha sonra değeri değiştirilebilir, fakat tekrar en baştan tanımlanamaz.
+
+let greeting = "say Hi";
+greeting = "say Hello instead";  // Bu durumda bir hata yok.
+
+let greeting = "say Hi";
+let greeting = "say Hello instead"; // error: Identifier 'greeting' has already been declared => Burada hata olur.
+
+console.log(greeting);
+
+*/
+
+let b, c;
+(b = 10), (c = 20);
+console.log(a + b);
+
+
+
+/*   const keyword: 
+
+- const anahtar kelimesi ingilizce'deki constant(sabit) kelimesinden geliyor. Yani biz const ile bir değişkene değer 
+atadığımız zaman daha sonra o değişkenin değerini alt satırlarda değiştiremiyoruz.
+
+- const ile tanımlanan değişkenler block scoped olarak geçer. Yani const ile tanımlanmış değişkene sadece tanımlandığı
+block içerisinde erişebiliriz. 
+
+-- const cannot be updated or re-declared:
+
+- const ile tanımlanmış bir değişkenin değeri daha sonra tekrar değiştirilemez veya o değişken tekrar tanımlanamaz.
+
+const greeting = "say Hi";
+greeting = "say Hello instead";   // error: Assignment to constant variable. 
+
+
+const greeting = "say Hi";
+const greeting = "say Hello instead";   // error: Identifier 'greeting' has already been declared
+
+- Ayrıca const ile tanımlanmış bir değişkenin değeri tanımlanırken atanmalıdır, değişkeni tanımlayıp alt satırlarda değer vermek istersek
+yine hata alırız:  
+
+const a;
+a = 25;  // error
+ 
+*/
+
