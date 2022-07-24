5d6d35561956d9262a3b732db440b0d7a9b8c11f
Berke Sayın <sayinberkesayin@gmail.com>
2022-07-24 21:42:25 +0300

    Study math object.

--- 4.javascript/13.js-intro-part-1/89.change-data-types/app.js
+++ 4.javascript/13.js-intro-part-1/89.change-data-types/app.js
@@ -1,65 +1,95 @@
-/*biz programlama dünyasında bazı yerlerde bazı veri tiplerini birbirine dönüştürmek isityor olabiliriz. Örneğin bir tane sayıyı stringe ya da
-bir tane stringi sayıya dönüştürmek istiyor olabiliriz. bu dersimizde bu işlemlerimizi öğrenmeye çalışacağız.*/ 
+// 89.Veri Tiplerini Birbirine Dönüştürme:
+
+/*
+
+- Biz programlama, uygulama geliştirme sürecinde bazı yerlerde bazı veri tiplerini birbirine dönüştürmek isteyebiliriz. 
+Örneğin bir sayıyı stringe ya da bir stringi sayıya dönüştürmek istiyor olabiliriz. 
+Şimdi de farklı veri tiplerini birbirine dönüştürmeye bakalım.
+
+*/ 
 
 
-//Farklı veri tiplerini, fonksiyonları, array'leri stringe çevirme işlemleri//
+// let keyword'ü ile 4 farklı değşken tanımlayalım:
 let value, value2, value3, value4;
 
-/*biz bir sayıyı string'e çevirmek istersek eğer o durumda String() foksiyonunu kullanabiliriz.*/ 
-value = String(123);
-console.log(value);
-console.log(typeof value);
+
+// Eğer bir sayıyı string'e çevirmek istersek, o durumda String() foksiyonunu kullanabiliriz.   
+value = 123;
+console.log(value);  // 123
+console.log(typeof value);  // number
 
 
-//bir boolean değeri de bir string ifadeye dönüştürebiliriz.
+// value değişkeni number bir değişkendi, şimdi onu stirng'e çevirelim:
+value = String(123); 
+console.log(value);  // 123
+console.log(typeof value);  // string
+
+
+// Bir boolean değeri de bir string ifadeye dönüştürebiliriz.
 value2 = String(true);
-console.log(value2);
-console.log(typeof value2);
+console.log(value2);  // true
+console.log(typeof value2);   // string
 
-//biz istersek bir fonksiyonuda bir stringe çevirebiliriz.
-value3 = String(function() {console.log()});
+
+// Bir fonksiyonu da bir stringe çevirebiliriz.
+value3 = String(function() 
+    {
+        console.log()
+    }
+);
 console.log(value3);
-console.log(typeof value3);
+console.log(typeof value3);  // string
 
-//biz istersek bir array'i de bir stringe çevirebiliriz.
+
+// Bir array'i de bir stringe çevirebiliriz.
 value4 = String([1,2,3,4,5]);
 console.log(value4);
-console.log(typeof value4);
-//yani biz kısaca her şeyi stringe çevirebiliriz.
+console.log(typeof value4);  // string
+
+// Yani String() fonksiyonu ile her şeyi stringe çevirebiliriz.
 
 
 //biz bir sayıyı stringe toString() fonksiyonu ile de çevirebiliriz.
 let value5;
 
 value5 = (10).toString();
-console.log(value5);
-console.log(typeof value5);
-//////////////////////////////////////////////////////////////////////////////////////
+console.log(value5);  // 10
+console.log(typeof value5);  // string
 
 
-//Veritiplerini sayılara çevirme: bunun için Number() fonksiyonunu kullanırız.
-/**veri tiplerini sayılara çevirme aslında biraz kısıtlı.örneğin bir veri tipi sayılara çevrilemiyorsa burada hata alırız daha doğrusu 
- not a number şeklinde bir sonuç göreceğiz. */
+// Farklı Veri Tiplerini sayılara çevirme: bunun için Number() fonksiyonunu kullanırız.
+
+/* 
+
+- Veri Tiplerini sayılara çevirme aslında biraz kısıtlı. Örneğin bir veri tipi sayılara çevrilemiyorsa burada hata 
+alırız daha doğrusu 
+
+Bu durumda 'not a number' şeklinde bir sonuç görürüz. 
+
+*/
 
  let value6, value7, value8, value9;
  
- value6 = Number("123");  //çevrilir.
- console.log(value6);
- console.log(typeof value6);
+ value6 = Number("123");  
+ console.log(value6);   // 123
+ console.log(typeof value6);  // number
 
-value7 = Number(null); //0 gibi bir şey
-console.log(value7);
-console.log(typeof value7);
 
-value8 = Number(undefined);  //çevrilmez
-console.log(value8);
-console.log(typeof value8);
+value7 = Number(null); // null (değeri var ama boş)
+console.log(value7);   
+console.log(typeof value7);   // number
 
-value9 = Number("Hello World");  //çeviremez
-console.log(value9);
+
+value8 = Number(undefined);  // çevrilmez 
+console.log(value8);   // NaN = Not a Number! 
+console.log(typeof value8);   
+
+
+value9 = Number("Hello World");  // çeviremez
+console.log(value9);   // NaN = Not a Number!   
 console.log(typeof value9);
 
-//bir fonksiyonu da, arrayi de biz sayıya çeviremeyiz
+// bir fonksiyonu da, arrayi de biz sayıya çeviremeyiz, NaN = Not a Number! hatasını alırız.
 
 
 
--- 4.javascript/13.js-intro-part-1/90.operators-and-math-object/app.js
+++ 4.javascript/13.js-intro-part-1/90.operators-and-math-object/app.js
@@ -1,3 +1,5 @@
+// 90.Operatörler ve Math Objesinin Metotları:
+
 let value;
 
 const value1 = 10;
@@ -9,25 +11,45 @@ value = value1 + value2;
 value = value1 - value2;
 value = value1 * value2;
 value = value1 / value2;
-value = value1 % value2;  //10'un 4'e bölümünden kalan
+value = value1 % value2; // 10'un 4'e bölümünden kalan
 
-//Math object: js'deki math objesi içinde bir çok matematik fonksiyonu ve özelliği barındıran bir objedir.
+
+// Math object: js'deki math objesi, içinde bir çok matematik fonksiyonu ve özelliği barındıran bir objedir.
+
+
+/*   Math Object at JS, some functions:
+
+- Math.PI;
+- Math.E;
+- Math.round(4.6);
+- Math.ceil(3.2);
+- Math.floor(3.7);
+- Math.sqrt(16);
+- Math.abs(-10);
+- Math.pow(5, 3);
+- Math.max(7, 2, 57, -15);
+- Math.min(7, 2, 57, -15);
+- Math.random();
+
+*/
+
+
 value = Math.PI; // pi sayısını verir
-value = Math.E; //logaritma E sayısını verir.
-value = Math.round(4.6); // sayıları yuavarlamaya yarar. yukarı yuvarlar
-value = Math.round(4.5); // sayıları yuavarlamaya yarar. yukarı yuvarlar
-value = Math.round(4.4); // sayıları yuavarlamaya yarar. aşağı yuvarlar
-value = Math.ceil(3.2); // bütün ondalıklı sayıları yukarı yuvarlar.
-value = Math.floor(3.7); // bütün ondalıklı sayıları aşağı yuvarlar.
-value = Math.sqrt(16); //sayıların karekökünü almaya yarar.
-value = Math.abs(-10); //sayıların mutlak değerini almaya yarar.
-value = Math.pow(5,3) ; // üslü sayılar // 5 in 3. kuvveti = 5*5*5 =125
-value = Math.max(10,-1,100,32);  //maximum sayıyı bulur
-value = Math.min(10,-1,100,32);  //minimum sayıyı bulur
-value = Math.random(); //0 ile 1 arasında bir değer üretir.
-value = Math.random() * 20; //0 ile 20 arasında bir değer üretir.
-value = Math.random() * 20 + 1; //1 ile 21 arasında bir değer üretir.
-value = Math.floor(Math.random() * 20 + 1); //1 ile 21 arasında tamsayı bir değer üretir.
-
+value = Math.E; // logaritma E sayısını verir.
+value = Math.round(4.6); // sayıları yuavarlamaya yarar, yukarı yuvarlar
+value = Math.round(4.5); // sayıları yuavarlamaya yarar, yukarı yuvarlar
+value = Math.round(4.4); // sayıları yuavarlamaya yarar, aşağı yuvarlar
+value = Math.ceil(3.2); // bütün ondalıklı sayıları yukarı yuvarlar
+value = Math.floor(3.7); // bütün ondalıklı sayıları aşağı yuvarlar
+value = Math.sqrt(16); // sayıların karekökünü almaya yarar    =>   4
+value = Math.abs(-10); // sayıların mutlak değerini almaya yarar   =>   10
+value = Math.pow(5, 3); // üslü sayılar  =>   5 in 3. kuvveti = 5 * 5 * 5 = 125
+value = Math.max(10, -1, 100, 32); // maximum sayıyı bulur
+value = Math.min(10, -1, 100, 32); // minimum sayıyı bulur
+value = Math.random(); // 0 ile 1 arasında bir değer üretir.
+value = Math.random() * 20; // 0 ile 20 arasında bir değer üretir.
+value = Math.random() * 20 + 1; // 1 ile 21 arasında bir değer üretir.
+value = Math.floor(Math.random() * 20 + 1); // 1 ile 21 arasında tamsayı bir değer üretir.
 
 console.log(value);
+
