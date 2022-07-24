a4cd5ec1b754278ce7d572c3043600b0d84cadca
Berke Sayın <sayinberkesayin@gmail.com>
2022-07-24 21:59:26 +0300

    Study string methods.

--- 4.javascript/13.js-intro-part-1/91.string-methods/app.js
+++ 4.javascript/13.js-intro-part-1/91.string-methods/app.js
@@ -1,43 +1,64 @@
+// 91.String Metodları
+
+
+/*  JS, Some String Methods;
+
+- length
+- concat()
+- toLowerCase()
+- toUpperCase()
+- indexOf()
+- split()
+- replace()
+- includes()
+
+*/
+
+
 let value;
 
 const firstName = "Louis";
 const lastName = "Armstrong";
-const languages = "Java,Python,C++";
+const languages = "Java, Python, C++";
 
+value = firstName + lastName;  // LouisArmstrong
+value = firstName + " " + lastName;  // Louis Armstrong
 
-value = firstName + lastName;
-value = firstName + " " + lastName;
+value = "Berke ";
+value = value + "Sayın";
 
-value = "Mustafa Murat ";
-value = value + "Coşkun";
+// length
+value = firstName.length; // firstName değişkeninin tuttuğu string içerisinde kaç tane karakter olduğunu bulmak için kullanırız
 
-value = firstName.length; //firstName değişkeninin tuttuğu string içerisinde kaç tane karakter olduğunu bulma.
+// concat()
+value = firstName.concat(" ", lastName, " Caz"); // firstName'e string eklemek için concat() fonksiyonunu kullanırız
+// Louis Armstrong Caz
 
-value = firstName.concat(" ", lastName, " Caz");  // firstName'e string eklemek için concat() fonksiyonunu kullanırız.
+// toLowerCase()
+value = firstName.toLowerCase(); // string içerisindeki bütün harfleri küçültmek için kullanırız
 
-value = firstName.toLowerCase();  //string içerisindeki bütün harfleri küçült
+// toUpperCase()
+value = firstName.toUpperCase(); //string içerisindeki bütün harfleri büyütmek için kullanırız
 
-value = firstName.toUpperCase();  //string içerisindeki bütün harfleri büyüt.
+value = firstName[0]; // 0. index'te hangi karakter varsa onu getir
+value = firstName[2]; // 2. index'te hangi karakter varsa onu getir
+value = firstName[3]; // 3. index'te hangi karakter varsa onu getir
+value = firstName[firstName.length - 1]; // son index'te hangi karakter varsa onu getir
 
-value = firstName[0];  //0. index'te hangi karakter varsa onu getir
-value = firstName[2];  //2. index'te hangi karakter varsa onu getir
-value = firstName[3];  //3. index'te hangi karakter varsa onu getir
-value = firstName[firstName.length - 1];  //son index'te hangi karakter varsa onu getir
+// indexOf
+value = firstName.indexOf("L"); // L karakteri kaçıncı index'te
 
-value=firstName.indexOf("L");  //L karakteri kaçıncı index'te
+// charAt()
+value = firstName.charAt(0); // 0.indexte hangi karakter var
+value = firstName.charAt(firstName.length - 1); // son indexte hangi karakter var
 
-value=firstName.charAt(0); //0.indexte hangi karakter var
-value=firstName.charAt(firstName.length - 1); //son indexte hangi karakter var
+// Split
+value = languages.split(","); // languages değişkeninin tuttuğu bütün elemanları virgül ile ayırıp bir array içerisinde bize göster
 
+// Replace
+value = languages.replace("Python", "C#"); // Python elemanını C# elemanı ile değiştir
 
-//Split
-value = languages.split(",");  // languages değişkeninin tuttuğu bütün elemanları virgül ile ayırıp bir array içerisinde bize göster
-
-//Replace
-value = languages.replace("Python","C#");  // Python elemanını C# elemanı ile değiştir
-
-//Includes
+// Includes
 value = languages.includes("Java"); // eğer Java elamanı languages'ta varsa bize true dönecek yoksa false dönecek
-
 
 console.log(value);
