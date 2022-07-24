97f87b1b14b0915aa7302035838584cdb9f5e987
Berke Sayın <sayinberkesayin@gmail.com>
2022-07-24 14:34:02 +0300

    Study Data Types and Variables.

--- 4.javascript/13.js-intro-part-1/87.data-types/app.js
+++ 4.javascript/13.js-intro-part-1/87.data-types/app.js
@@ -1,82 +1,127 @@
-/*değişkenler bizim programlama yaparken bir veri tutacağımız zaman kullandığımız en temel birimlerdir ve javascript'te değişkenlerimiz
-var, let ve constant ile oluşturulabiliyor.bu derste var tipini göreceğiz.*/
+// 87.Variables and Data Types for JavaScript
+
+/*
+
+Değişkenler bizim programlama yaparken bir veri tutacağımız zaman kullandığımız en temel birimlerdir ve javascript'te 
+değişkenlerimiz var, let ve const ile oluşturulabiliyor. Bu derste var veri tipi ve değişkenler üzerinde çalışalım.
+
+*/
+
+// Değişken Oluşturma: a, b ve c isimlerinde değişkenler oluşturalım
 var a = 20;
 var b = 10;
 var c = 40;
 
-console.log(a,b,c);
-console.log(a+b+c);
+console.log(a, b, c);
+console.log(a + b + c);
 
-/*javascript'te bir değişken oluşturduğumuz zaman bu değişkenimiz herhangi bir veri tipinde değer tutabilir. örneğin metinsel veri tipleri
-yazılarımız, string veri tipimiz */
+/*
 
-/*Javascript'te 2 tür veri tipimiz bulunuyor.*/
+- JavaScript'te bir değişken oluşturduğumuz zaman bu değişkenimiz herhangi bir veri tipinde değer tutabilir. 
+Örneğin metinsel veri tipleri: Yazılarımız, 'string' veri tipimiz. */
 
-/*Primitive(ilkel) Veri Tipleri: örneğin bir sayı, bir yazı, undefined, null veya boolean veri tiplerimiz olabilir. */
-//Primitive/Number
-var d = 10; //dinamik bir tipleme: js otomatik olarak d'nin veri tipini number olarak gösteriyor: Number Veri Tipi
+// Javascript'te 2 tür veri tipimiz bulunuyor. Primitive ve Reference Veri Tipleri:
+
+/* Primitive(ilkel) Veri Tipleri:  
+
+- Number
+- String
+- Boolean
+- Null 
+- Undefined
+
+*/
+
+// Primitive: Number ()
+var d = 10; // dinamik bir tipleme: Js otomatik olarak d'nin veri tipini number olarak gösteriyor: Number Veri Tipi
 console.log(d);
-console.log(typeof d);
+console.log(typeof d); // number
 
-var e = 3.14;  //Number Veri Tipi
+var pi = 3.14; //Number Veri Tipi
 
-//Primitive/String: js'de yazılarımız
-var name = "Mustafa";
+// Primitive: String (yazılar, metinsel ifadeler)
+var name = "Berke";
 console.log(name);
-console.log(typeof name);
+console.log(typeof name); // string
 
-//Primitive/Boolean: genelde if koşullarında ya da döngülerde kullanırız . sadece true ya da false değerini alabilir
+// Primitive: Boolean (genelde if koşullarında ya da döngülerde kullanılır, sadece true ya da false değerini alabilir)
 var f = true;
 console.log(f);
-console.log(typeof f);
+console.log(typeof f); // boolean
 
-//Primitive/Null
-var g = null; // bu g'nin hiçbir değer taşımadığını yani boş bir değer taşıdığını söylüyor.
+//Primitive: Null
+var g = null; // bu g isimli değişkenin hiçbir değer taşımadığını yani boş bir değer taşıdığını söylüyor.
 console.log(g);
 console.log(typeof g); //karşımıza object çıkar
 
-
-//undefined
+// Primitive: Undefined
 var h;
-console.log(h); //karşımıza undefined olarak çıkar. yani buna hiç değer verilmemiş
+console.log(h); //  karşımıza undefined olarak çıkar. yani buna hiç değer verilmemiş
+// Bir değişken oluşturduğumuzda eğer o sırada veya sonrasında değer ataması yapmazsak bize undefined döner.
 
+/* Reference Veri Tipleri : 
 
-/*Reference Veri Tipleri : array'ler, fonksiyonlar, object'ler, date-object'ler bir referans tipidir. Referans tiplerin hepsi birer object'tir
-mesela aşağıdaki örenekte numbers referans ve biz o değerlere numbers üzerinden erişebiliyoruz.*/
-//Reference/Arrays
-var numbers = [1,2,3,4,5];
-console.log(numbers);
-console.log(typeof numbers);
-console.log(numbers[0]);
+- Array'ler  
+- Fonksiyonlar 
+- Object'ler  (Örneğin date-object'ler) 
 
+- Referans tiplerin hepsi birer object'tir
 
-//Referans/Objects
-//Bir de mesela javascript'te kendi obje'lerimizi oluşturabiliriz.
+*/
+
+// Reference: Arrays
+var numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers); // console'da numbers array'ini yazdırır.
+console.log(typeof numbers); // object
+
+console.log(numbers[0]); // prints: 1 (0.index'teki değer)
+console.log(numbers[1]); // prints: 2 (1.index'teki değer)
+console.log(numbers[2]); // prints: 3 (2.index'teki değer)
+
+// Referans: Objects  (Javascript'te tüm object'ler referans tiplidir)
+
+// JS'de hazır js objelerine ek olarak kendimiz de kendi obje'lerimizi oluşturabiliriz.
+
+// person isimli bir obje oluşturalım:
 var person = {
-    name : "Mustafa",
-    age : 25
-}
-console.log(person);
-console.log(typeof person);
-//javascript'te tüm object'ler referans tiplidir
+  name: "Berke",
+  age: 20,
+};
 
-//Reference/date-object
-//date-object : zaman işlemleri için date-object oluştururuz.
-var date = new Date();
+console.log(person); // person objesini yazdırır.
+console.log(person.name); // person objesinin name'ini yazdırır. (Berke)
+console.log(person.age); // person objesinin age'ini yazdırır. (20)
+console.log(typeof person); // object
+
+// Reference: date-object (zaman işlemleri için date-object oluştururuz)
+var date = new Date(); // date isimli bir date-object oluşturuyoruz.
 console.log(date);
-console.log(typeof date);
+console.log(typeof date); // object
 
+// Reference: Functions
 
-//Reference/Functions
-//biz daha sonra javascript'te fonksiyonlarımızı tanımlayacağız.
-var merhaba = function(){
-    console.log("Merhaba");
-}
+// merhaba isimli bir function:
+var merhaba = function () {
+  console.log("Merhaba");
+};
 
-console.log(merhaba);
-console.log(typeof merhaba);
+merhaba(); // oluşturduğumuz fonksiyonu bu şekilde çalıştırabiliriz.
 
-/*biz primitive ve referans veri tiplerini gördük, şimdi bunların farkını anlamaya çalışalım. primitive veri tipleri sadece o değişkenin
-değeridir.ancak bizim referans veri tiplerimiz referanslar üzerinden bellekte bir yeri gösteriyor ve biz o yeri gösterdikten sonra 
-değerlerimizi alabiliyoruz.*/ 
+console.log(merhaba); // merhaba fonksiyonunu console'da yazdırır.
+console.log(typeof merhaba); // function
+
+/* 
+
+- Şimdiye kadar Primitive ve Referans Veri Tiplerini gördük, şimdi bunların farkını anlamaya çalışalım. 
+
+- Primitive veri tipleri sadece o değişkenin değeridir. Ancak bizim Referans Veri Tiplerimiz referanslar üzerinden bellekte 
+bir yeri gösteriyor ve biz o yeri gösterdikten sonra o değerlerimizi alabiliyoruz, ulaşabiliyoruz.
+
+*/
+
 var a = 10; // burada a sadece 10 değerini taşır yani herhangi bir referans değildir
+
+var b = a; // b'ye 10 değerini atar.
+
+console.log(a, b); // 10 10
