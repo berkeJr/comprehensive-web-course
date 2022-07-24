95bd78e21daff9727c444dec9d67aac55d2a7e80
Berke Sayın <sayinberkesayin@gmail.com>
2022-07-24 22:38:54 +0300

    Study arrays and array methods.

--- 4.javascript/13.js-intro-part-1/93.arrays/app.js
+++ 4.javascript/13.js-intro-part-1/93.arrays/app.js
@@ -1,66 +1,98 @@
-//bu dersimizde array'lerimizi ve metodlarımızı öğrenmeye çalışacağız
-//bir array'in içerisinde sayı olabilir, string olabilir
+// 93.Array'lerin Özellikleri
+
+// Şimdi de array'lere ve array metodlarımıza bakalım.
+
+// Not: Bir array'in içerisinde sayı olabilir, string olabilir.
 
 let value;
 
-const numbers = [43,56,48,12,25,3,56];  //array oluşturma yöntemi
+const numbers = [43, 56, 48, 12, 25, 3, 56]; // array oluşturma yöntemi  (numbers isimli array)
 
-//const numbers2 = new Array(1,2,3,4,5,6,7);  //bu şekilde de array oluşturabiliriz.
+const numbers2 = new Array(1, 2, 3, 4, 5, 6, 7); // bu şekilde de array oluşturabiliriz   (numbers2 isimli array)
 
-const langs = ["Python", "Java", "C++", "Javascript"];
+const langs = ["Python", "Java", "C++", "Javascript"];  // (langs isimli array)
 
-const a = ["Merhaba",22, null, undefined, 3.14]
+const a = ["Merhaba", 22, null, undefined, 3.14];  // (a isimli karışık veri tiplerinden eleman bulunduran array)
 
-//Uzunluk
-value = numbers.length;   //numbers arrayindeki eleman sayısı
 
-//İndexlenme
+// Array'in uzunluğu, eleman sayısı:
+value = numbers.length; // numbers arrayindeki eleman sayısı
+
+
+// Array içerisinde istenilen index değerine sahip elemana erişme:
 value = numbers[0];
 value = numbers[3];
-value = numbers[numbers.length-1];  //son index
+value = numbers[numbers.length - 1]; // son eleman, son index
 
-//herhangi bir indexteki değeri değiştirme
+
+// Herhangi bir indexteki değeri değiştirme, yeni değer verme:
 value = numbers[2] = 1000;
 
-//index of : bir değerin hangi index'te olduğunu bulmaya yarar.
+
+// indexOf : Bir değerin hangi array içerisinde hangi index'te olduğunu bulmaya yarar.
 value = numbers.indexOf(1000);
 
-//Arrayin sonuna değer ekleme
+
+// Arrayin sonuna yeni değer ekleme: push()
 numbers.push(2000);
 value = numbers;
 
-//Arrayin başına değer ekleme
+
+// Arrayin başına değer ekleme: unshift()
 numbers.unshift(3000);
 value = numbers;
 
-//Arrayin sonundan değer silme
+
+// Arrayin sonundaki değeri(son elemanı) silme: pop()
 numbers.pop();
 value = numbers;
 
-//Arrayin başından değer silme
+
+// Arrayin başındaki değeri(ilk elemanı) silme: shift()
 numbers.shift();
 value = numbers;
 
-//Array'den 0. ,  1. ve de 2. index deki değerleri silmek istersek
-numbers.splice(0,3); 
+
+// Array'den 0., 1. ve de 2. index'deki değerleri silmek istersek: splice()
+numbers.splice(0, 3);
 value = numbers;
 
-//Arrayi tam tersine çevirmek istiyorsak eğer
+
+// Arrayi tam tersine çevirmek istiyorsak: reverse()
 numbers.reverse();
 value = numbers;
 
-//Dizidekş elemanları her elemanın ilk rakamına göre küçükten büyüğe doğru sıralama
+
+// Dizideki elemanları her elemanın ilk rakamına göre küçükten büyüğe doğru sıralama: sort()
 numbers.sort();
 value = numbers;
 
-//dizideki elemanları küçükten büyüğe doğru sort ile sıralamak için fonksiyon kullanırız
-value = numbers.sort(function(x,y){
-    return x-y;
+
+// Dizideki elemanları küçükten büyüğe doğru sort ile sıralamak için fonksiyon kullanırız:
+value = numbers.sort(function (x, y) {
+  return x - y;
 });
+
 
 //dizideki elemanları büyükten küçüğe doğru sort ile sıralamak için fonksiyon kullanırız
-value = numbers.sort(function(x,y){
-    return y-x;
+value = numbers.sort(function (x, y) {
+  return y - x;
 });
 
+
 console.log(value);
+
+
+/*   JS, Some Array Functions
+
+- length
+- indexOf()
+- push()
+- unshift()
+- pop()
+- shift()
+- splice()
+- reverse()
+- sort()
+
+*/ 
