788a22d5d65be53dba46d90e7ded3b068bfd1e34
Berke Sayın <sayinberkesayin@gmail.com>
2022-07-24 22:21:18 +0300

    Study template literal

--- 4.javascript/13.js-intro-part-1/92.template-literal/app.js
+++ 4.javascript/13.js-intro-part-1/92.template-literal/app.js
@@ -1,19 +1,58 @@
-//Template Literal - String oluşturmakta yeni standart
+// 92.Template Literal
 
-/**Bu derste JavaScript'e EcmaScript6 ile gelen template literal özelliğine bakacağız. Bu özellik stringleri daha özenli bir 
- * şekilde yazmamızı sağlıyor. */
+/*
 
-//Örneğin 2 tane string 1 tane de number olsun. 
-
-const name = "Mustafa Murat Coşkun";
-const deparment = "Bilişim";
-const salary = 4000;
-
-//Şimdi bu 3 değişkeni bir stringde depolamak istiyoruz. 
-// const a = "İsim: " + name + "\n" + "Departman: " + deparment + "\n" + "Maaş: " + salary + "\n";
-//console.log(a);
+- Şimdi de JavaScript'e EcmaScript6 ile gelen "Template Literal" özelliğine bakalım. Bu özellik stringleri daha özenli bir 
+  şekilde yazmamızı sağlıyor. 
+  
+*/
 
 
-const a = `İsim:${name}\nDeparman:${deparment}\nMaaş:${salary}`;
-console.log(a);
+// Örneğin 2 tane string 1 tane de number olsun. 
+
+const name = "Berke Sayın";
+const department = "Yazılım";
+const salary = 10000;
+
+// Şimdi bu 3 değişkeni bir stringde tutmak istiyoruz. Eski yöntemle şu şekilde yazarız.
+// const info = "İsim: " + name + "\n" + "Departman: " + department + "\n" + "Maaş: " + salary + "\n";
+// console.log(info);
+
+
+// const info = `İsim: ${name}\nDepartman: ${department}\nMaaş: ${salary}`;
+
+// ${} => Bu ifadenin içerisine daha önce tanımlanan değişken adı yazılır.
+
+// console.log(info);
+
+
+
+// Web sayfasında bir liste oluşturmak için;
+const html = "<ul> " + 
+             "<li>" + name + "</li>" +
+             "<li>" + department + "</li>" +
+             "<li>" + salary + "</li>" +
+             "</ul>";
+
+// document.body.innerHTML = html;
+
+// Eski yöntemle yukarıdaki şekilde oluşturabiliriz.
+
+
+
+const team = "Liverpool";
+const subject = "Football";
+const league = "England";
+
+
+// Template Literal ile bu şekilde oluşturabiliriz.
+const html2 = `
+      <ul>
+       <li>${team}</li>
+       <li>${subject}</li>
+       <li>${league}</li>
+      </ul>
+     `;
+
+document.body.innerHTML = html2;
 
