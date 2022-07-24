d7e8b76ba1f1ea7fd5ee80aa65f8f954fbef997c
Berke Sayın <sayinberkesayin@gmail.com>
2022-07-24 13:24:18 +0300

    Study web developer tools.

--- 4.javascript/13.js-intro-part-1/85.js-intro/app.js
+++ 4.javascript/13.js-intro-part-1/85.js-intro/app.js
@@ -1,1 +1,32 @@
-alert("Javascript'e Hoşgeldiniz/Berke Sayın Full-Stack Web Developer");
+// 85.JavaScript Intro
+
+/* 
+
+Bizim js dosyalarımızı/kodlarımızı bir html sayfasında kullanabilmemiz için 3 farklı yöntem bulunuyor. 
+
+- 1.Yöntem: html dosyasında body'de en altta script tag'ları arasında yazacağımız her kod bir javascript kodu olarak algılanır:
+
+    <script>
+        alert("Javascript'e Hoşgeldiniz!");
+    </script>
+    
+- 2.Yöntem: JS dosyalarımızı internetten yüklemek. İnternette cloud'larda değişik js dosyaları bulunuyor. Bunun en temel örneği jquery 
+    olabilir. Jquery'yi biz istersek bilgisayarımıza indirebiliriz veya jquery.org üzerinden biz buna direk erişim sağlayabiliriz.
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+    crossorigin="anonymous"></script>
+
+    Bu yöntem dışarıdan bir js dosyasını dahil etmek şeklinde adlandırılır. Burada Jquery'yi indirmeden dahil etmiş olduk.
+
+- 3.Yöntem: Bizim kurs boyunca kullanacağımız yöntem ise app.js isimli dosya içerisinde js kodlarımızı yazıp daha sonra o dosyayı 
+    index.html'de dahil etmek olacaktır. bunun için index.html'de script tag'ları arasında src="app.js" şeklinde dahil etmemiz 
+    gerekiyor.
+
+    <script src="app.js"></script>
+    
+    Bu şekilde dahil ettikten sonra bizim app.js içinde yazdığımız kodlar çalışmış olacak.
+   
+*/
+
+alert("Javascript'e Hoşgeldiniz / Berke Sayın");
+
--- 4.javascript/13.js-intro-part-1/86.web-developer-tools/app.js
+++ 4.javascript/13.js-intro-part-1/86.web-developer-tools/app.js
@@ -1,14 +1,40 @@
+// 86.Web Developer Tools and Console: 
+
+
+/*  
+
+- Web geliştirme aracı bizim browser'larda Javascript, Html, Css kodlarımızı, etiketlerimizi denemek için kullandığımız ve 
+mutlaka her web programcısının kullanması gereken araçlardır. Bu aracı açmak için index.html sayfasını open with live server 
+deyip tarayıcı ile açtıktan sonra tarayıcıda f12'ye basarak açabiliriz. 
+
+*/
+
+/* Web Developer Tools içerisinde bulunan araçlar: 
+
+- Elements: index.html'deki elementleri burada görebiliriz. 
+- Console: Herhangi bir js kodunu web sayfamızda çalıştırabiliriz.
+- Sources: Burada çalıştığımız klasör içerisinde hangi dosyalar var, dosya içerikleri neler, bunları görebiliriz. 
+- Application: Burada tarayıcıda depolama alanı olarak kullanılan Local Storage ve Session Storage kısımlarını kullanabiliriz. 
+- Network, Performance, Memory, Security gibi kısımları da ihtiyaç durumunda kullanabiliriz. 
+
+*/
+
+// Console'da çalışmasını istediğimiz kodları hem burada(app.js) hem de web dev tools'da console alanında yazabiliriz.
+
 // console.log("Merhaba Javascript");
+
 // console.log(true);
 // console.log(false);
 
 // var a = 10;
 // console.log(a);
 
-// console.log(["Ahmet", "Mehmet"]);
+ console.log(["Ahmet", "Mehmet"]);  // console'da 2 elemanlı bir array yazdıralım.
 
-console.log(typeof a);
+// console.log(typeof a);  // a deişkeninin tipini, türünü yazdırmak için kullanılır. type = number
 
-console.warn("bu bir uyarıdır.")
-console.error("bu sayfa bulunamadı.")
-console.clear();
+// console.warn("bu bir uyarıdır.")  // console'da uyarı yazdırmak için bu fonksiyon kullanılır.
+
+// console.error("bu sayfa bulunamadı.")  // console'da hata (error) yazdırmak için bu fonksiyon kullanılır.
+
+// console.clear();
